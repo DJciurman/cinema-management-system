467f33806c6ba3e7f3e9940a80a687fff02a5ba3
Mateusz Ciura <mat107@interia.pl>
2022-01-25 18:38:56 +0100

    Add account management

--- src/main/java/com/pk/projekt/user/LoginController.java
+++ src/main/java/com/pk/projekt/user/LoginController.java
@@ -1,23 +1,26 @@
 package com.pk.projekt.user;
 
-import com.pk.projekt.actor.Actor;
 import com.pk.projekt.actor.ActorRepository;
 import com.pk.projekt.director.DirectorRepository;
-import com.pk.projekt.genre.Genre;
 import com.pk.projekt.genre.GenreRepository;
 import com.pk.projekt.movie.Movie;
 import com.pk.projekt.movie.MovieRepository;
 import com.pk.projekt.studio.StudioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collection;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
 
 @Controller
@@ -43,9 +46,48 @@ public class LoginController {
 
   @GetMapping("/login")
   private String loadPage(Model model) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (!(authentication instanceof AnonymousAuthenticationToken)) {
+      String name = SecurityContextHolder.getContext().getAuthentication().getName();
+      User user = repoUser.findUserByName(name);
+      model.addAttribute("user", user);
+      return "logout";
+    }
     model.addAttribute("user", new User());
     model.addAttribute("error", "huehue");
     return "login";
+  }
+
+  @PostMapping("/manageData")
+  private String manageDataProcess(Model model, User user, HttpServletRequest request, HttpServletResponse response,@RequestParam("operation") String operation, @RequestParam("password") String password) {
+    if (operation.equals("edit")) {
+      String name = SecurityContextHolder.getContext().getAuthentication().getName();
+      User logged = repoUser.findUserByName(name);
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      if (encoder.matches(password, logged.getPassword()))
+      {
+        logged.setEmail(user.getEmail());
+        logged.setFirstName(user.getFirstName());
+        logged.setLastName(user.getLastName());
+        logged.setPhoneNumber(user.getPhoneNumber());
+
+        try {
+          repoUser.save(logged);
+        } catch (Exception e) {
+
+        }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        new SecurityContextLogoutHandler().logout(request, response, auth);
+        model.addAttribute("user", new User());
+        model.addAttribute("error", "huehue");
+        return "login";
+      }
+    }
+    String name = SecurityContextHolder.getContext().getAuthentication().getName();
+    user = repoUser.findUserByName(name);
+    model.addAttribute("user", user);
+    return "logout";
   }
 
   @PostMapping("/register")
